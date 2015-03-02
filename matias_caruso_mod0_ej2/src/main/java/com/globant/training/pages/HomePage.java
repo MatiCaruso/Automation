a296d28930c8c3809908fdb142ccb6b201426685
MatiCaruso <matias.caruso@globant.com>
2015-03-02 16:28:24 -0300

    Ej1 terminado

--- matias_caruso_mod0_ej2/src/main/java/com/globant/training/pages/HomePage.java
+++ matias_caruso_mod0_ej2/src/main/java/com/globant/training/pages/HomePage.java
@@ -1,5 +1,56 @@
 package com.globant.training.pages;
 
+import java.util.List;
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.pagefactory.ByAll;
+
+
 public class HomePage {
 
+	
+	@FindBy(id="headerStandard")
+	private WebElement header;
+	
+//	@FindBy(className="signInLink socialLogin")
+//	private WebElement button;
+			
+	@FindBy(className="welcomeText")
+	private WebElement welcome;
+
+	public void go(WebDriver driver) {
+		driver.get("http://www.cheaptickets.com/");
+	}
+
+	public void signIn() {
+		
+		List<WebElement> lista = header.findElements(ByAll.className("link"));
+		
+		int i=0;
+		while (i <= lista.size()){
+			System.out.println(i);
+			if ("Sign in".equals(lista.get(i).getText())){
+				lista.get(i).click();
+				System.out.println("click");
+				break;
+			}
+			i++;
+		}
+		
+
+	}
+
+	public boolean welcome(WebDriver driver) {
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		if (welcome.isDisplayed()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
--- matias_caruso_mod0_ej2/src/main/java/com/globant/training/tests/Tests.java
+++ matias_caruso_mod0_ej2/src/main/java/com/globant/training/tests/Tests.java
@@ -1,5 +1,52 @@
 package com.globant.training.tests;
 
-public class Tests {
 
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.globant.training.pages.HomePage;
+import com.globant.training.pages.SignInPage;
+
+public class Tests {
+	WebDriver driver;
+	HomePage homePage;
+	
+	
+	@BeforeMethod
+	public void before() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		homePage = PageFactory.initElements(driver, HomePage.class);
+		homePage.go(driver);
+	}
+	
+	@AfterMethod
+	public void after(){
+		driver.quit();
+	}
+	
+	@Test
+	public void signIn() throws InterruptedException{
+		homePage.signIn();
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		SignInPage signPage = PageFactory.initElements(driver, SignInPage.class);
+		
+		signPage.signIn("m.villarruel.test@gmail.com", "Automation");
+		
+		Assert.assertEquals(homePage.welcome(driver), true);
+		
+	}
+	
 }

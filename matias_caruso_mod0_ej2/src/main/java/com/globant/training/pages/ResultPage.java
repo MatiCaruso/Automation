c5c50ff768ffec41d3e5f6adb760f9e6a21792bd
MatiCaruso <matias.caruso@globant.com>
2015-03-03 17:00:25 -0300

    actualizado a mitad del caso 5

--- matias_caruso_mod0_ej2/src/main/java/com/globant/training/pages/ResultPage.java
+++ matias_caruso_mod0_ej2/src/main/java/com/globant/training/pages/ResultPage.java
@@ -1,5 +1,7 @@
 package com.globant.training.pages;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -10,6 +12,18 @@ public class ResultPage {
 	private WebElement validateRefine;
 	@FindBy(xpath="//div[@data-context='airLowestPrice']")
 	private WebElement validateLowest;
+	@FindBy(xpath="//div[@class='bidirectionalSort']")
+	private WebElement sortBy;
+	@FindBy(xpath="//span[@class='departLocation']")
+	private WebElement depart;
+	@FindBy(xpath="//div[@class='matchingResults']")
+	private WebElement machingResults;
+	@FindBy(xpath="//div[@class='actionExpand titleBar hideFromNonJS trigger']")
+	private WebElement list;
+	@FindBy(xpath="//a[@data-wt-ti='airSort-Departure time-ascending']")
+	private WebElement selectList;
+	@FindBy(xpath="//div[@class='airResultsCard optimizedCard']")
+	private List<WebElement> resultList;
 	
 	public boolean validacion01(){
 		if("Refine Results".equals(validateRefine.findElement(By.tagName("h2")).getText())){
@@ -24,4 +38,41 @@ public class ResultPage {
 		}
 		return false;
 	}
+	public boolean validacion03(){
+		if("Sort by:".equals(sortBy.findElement(By.tagName("h2")).getText())){
+			return true;
+		}
+		return false;
+	}
+	public boolean validacion04(){
+		if("Las Vegas, NV".equals(depart.getText())){
+			return true;
+		}
+		return false;
+	}
+	public boolean validacion05(){
+		
+		if((machingResults.findElement(By.tagName("h2")).isDisplayed())){
+			return true;
+		}
+		return false;
+	}
+	public boolean validation(){
+		if(validacion01()&&validacion02()&&validacion03()&&validacion04()&&validacion05()){
+			return true;			
+		}else{
+			return false;
+		}
+	}
+	public void searchList(){
+		list.click();
+		selectList.click();
+	}
+	public void getFly() {
+		System.out.println(resultList.get(0).getText());
+		
+	}
+	
+	
+	
 }
--- matias_caruso_mod0_ej2/src/main/java/com/globant/training/tests/Tests.java
+++ matias_caruso_mod0_ej2/src/main/java/com/globant/training/tests/Tests.java
@@ -47,43 +47,43 @@ public class Tests {
 		driver.quit();
 	}
 	
-	@Test
-	public void signIn() {
-		homePage.signIn();
-		pausa(5);
-		wait(driver);
-		SignInPage signPage = PageFactory.initElements(driver, SignInPage.class);
-		wait(driver);
-		signPage.signIn("m.villarruel.test@gmail.com", "Automation");
-		
-		Assert.assertEquals(homePage.welcome(driver), true);
-		
-	}
-	
-	@Test
-	public void errorSignIn(){
-		homePage.signIn();
-		pausa(5);
-		wait(driver);
-		SignInPage signPage = PageFactory.initElements(driver, SignInPage.class);
-		signPage.signIn("banana@gmail.com", "Automation");
-		Assert.assertEquals(signPage.errorSignIn(),true);
-	}
-	
-	@Test
-	public void logout(){
-		homePage.signIn();
-		pausa(4);
-		wait(driver);
-		SignInPage signPage = PageFactory.initElements(driver, SignInPage.class);
-		wait(driver);
-		signPage.signIn("m.villarruel.test@gmail.com", "Automation");
-		wait(driver);
-		homePage.signout();
-		pausa(4);
-		LogoutPage logout= PageFactory.initElements(driver, LogoutPage.class);
-		Assert.assertEquals(logout.logout(),true);
-	}
+//	@Test
+//	public void signIn() {
+//		homePage.signIn();
+//		pausa(5);
+//		wait(driver);
+//		SignInPage signPage = PageFactory.initElements(driver, SignInPage.class);
+//		wait(driver);
+//		signPage.signIn("m.villarruel.test@gmail.com", "Automation");
+//		
+//		Assert.assertEquals(homePage.welcome(driver), true);
+//		
+//	}
+//	
+//	@Test
+//	public void errorSignIn(){
+//		homePage.signIn();
+//		pausa(5);
+//		wait(driver);
+//		SignInPage signPage = PageFactory.initElements(driver, SignInPage.class);
+//		signPage.signIn("banana@gmail.com", "Automation");
+//		Assert.assertEquals(signPage.errorSignIn(),true);
+//	}
+//	
+//	@Test
+//	public void logout(){
+//		homePage.signIn();
+//		pausa(4);
+//		wait(driver);
+//		SignInPage signPage = PageFactory.initElements(driver, SignInPage.class);
+//		wait(driver);
+//		signPage.signIn("m.villarruel.test@gmail.com", "Automation");
+//		wait(driver);
+//		homePage.signout();
+//		pausa(4);
+//		LogoutPage logout= PageFactory.initElements(driver, LogoutPage.class);
+//		Assert.assertEquals(logout.logout(),true);
+//	}
 	@Test
 	public void search(){
 		homePage.searchAir();
@@ -92,9 +92,11 @@ public class Tests {
 		wait(driver);
 		homePage.dates();
 		ResultPage resultPage = PageFactory.initElements(driver, ResultPage.class);
+		pausa(3);
 		wait(driver);
-		Assert.assertEquals(resultPage.validacion01(),true);
-		Assert.assertEquals(resultPage.validacion02(),true);
+		Assert.assertEquals(resultPage.validation(),true);
+		resultPage.searchList();
+		resultPage.getFly();
 		
 	}
 }
